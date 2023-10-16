9c7db24d6be160344afa6d5f86a5239a7f0ab875
Carlos <carlosbrandan503@gmail.com>
2023-10-16 19:01:26 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,104 @@
-import Image from 'next/image'
+'use client'
+import {useEffect,useState} from 'react'
+import pokemon from 'pokemontcgsdk'
+import { type SetDatum } from '@/interfaces/Sets'
+import { type Cards, type CardDatum } from '@/interfaces/Cards'
 
 export default function Home() {
+  pokemon.configure({apiKey: process.env.API_KEY})
+  const [cards, setCards] = useState<Array<CardDatum>>([])
+  const [sets, setSets] = useState<Array<SetDatum>>([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    getSets()
+  }, [])
+  
+  const getSets = ()=>{
+    pokemon.set.all()
+    .then((data: Array<SetDatum>)  => {
+        console.log(data)
+        let array = shuffled(data,5)
+        setSets(array)
+        setLoading(false)
+    })
+  }
+
+  const shuffled = <T,>(array: T[], lenght: number) => {
+    let arr = array
+    const shuffled = arr.sort(() => 0.5 - Math.random());
+    let selected = shuffled.slice(0, lenght);
+    return selected
+  }
+ 
+  const getCardsSet = async (set: string , total : number) =>{
+    setCards([])
+    setLoading(true)
+    let arrNumber = []
+    for(let i = 0; i < 5; i++){
+      arrNumber.push(Math.floor(Math.random() * total))
+    }
+
+    arrNumber.map((number)=>{
+      pokemon.card.where({q:`set.id:${set} number:${number}`,orderBy:'releaseDate',pageSize:5 })
+        .then((data: Cards) => {
+            setCards(prevArray =>[...prevArray, data.data[0]])
+            setLoading(false)
+        })
+
+    })
+    console.log(cards)
+  }
+  if(loading){
+    return (<>
+    <div className='flex w-100 h-screen justify-center items-center'>
+      <span className="loader"></span>
+    </div>
+    
+    </>)
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className='p-4'>
+      {cards && <>
+          <div className='flex justify-center '>
+            <p>Cards</p>
+            <p onClick={()=>setCards(cards => []) }>Reset</p>
+          </div>
+          <div className='flex flex-row  flex-wrap gap-3'>
+            {
+              cards.map((card,i)=>{
+                return (
+                  <div key={i}>
+                    <div>
+                      <img className='w-full h-full object-contain' src={card.images.small} alt={card.name} />
+                      </div>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </>
+      }
+      { sets && <>
+        <div className='flex justify-center '>
+          <p>Sets</p>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <div className='flex flex-row flex-wrap gap-3'>
+          {
+            sets.map((set,i)=>{
+              return (
+                <div onClick={()=>getCardsSet(set.id,set.total)} key={i}>
+                  <div className='h-[200px] w-[200px]'>
+                    <img src={set.images.logo} alt={set.name} />
+                    </div>
+                </div>
+              )
+            })
+          }
+        </div>
+      </>
+      }
     </main>
   )
 }
